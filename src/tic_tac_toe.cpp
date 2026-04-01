b2cf9daa0240e71c00a4dfab4cd857f506a513b7
Ohana-Souza <ohanasouza04@gmail.com>
2026-04-01 20:07:22 -0300

    exibiçaõ do tabuleiro está rapida, aumentei um pouco o tempo para exibição melhorar

--- src/tic_tac_toe.cpp
+++ src/tic_tac_toe.cpp
@@ -50,7 +50,7 @@ class TicTacToe {
     }
     std::cout << std::endl;
     //melhorar a exibição do tabuleiro
-    std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    std::this_thread::sleep_for(std::chrono::milliseconds(400));
   }
   
   bool make_move(char player, int row, int col) {
